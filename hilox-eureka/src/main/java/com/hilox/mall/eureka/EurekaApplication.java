497496de87951bb4a4f2af51159182c1ba70a2b3
zhaohaihao <zhh2933@gmail.com>
2019-04-25 01:20:57 +0800

    :art:eureka高可用服务

---
+++ hilox-eureka/src/main/java/com/hilox/mall/eureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.hilox.mall.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Hilox
+ * @description
+ * @date 2019-04-25 00:46
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
